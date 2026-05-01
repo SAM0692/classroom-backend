c6ebcb53e739367838d8e789e95c0cc373373117
SAM0692 <samuelam0692@gmail.com>
2026-05-01 09:43:32 -0400

    Implement fixes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const PORT = 8000;
 
 const app = express();
 
+if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file');
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
